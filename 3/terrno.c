ecf8f75f0c0a7ac9e9adc5ca872ee42f8de477ee
Yi Jun <yili0568@gmail.com>
2015-03-28 21:04:20 +0800

    Add chapter3  errno.h
    Signed-off-by: Yi Jun <yili0568@gmail.com>

---
+++ 3/terrno.c
@@ -0,0 +1,22 @@
+/* test errno macro */
+#include <assert.h>
+#include <math.h>
+#include <stdio.h>
+#include "errno.h"
+
+int main()
+{
+    assert(errno == 0);
+    perror("No error reported as");
+    errno = ERANGE;
+    assert(errno == ERANGE);
+    perror("Range error reported as");
+    errno = 0;
+    assert(errno == 0);
+    sqrt(-1.0);
+    assert(errno == EDOM);
+    perror("Domain error reported as");
+    puts("SUCCESS testing error.h");
+
+    return (0);
+}
