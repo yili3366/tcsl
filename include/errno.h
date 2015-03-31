945e1545554f47ca541a2b7ba163ef4e903b3e4a
Yi Jun <yili0568@gmail.com>
2015-04-01 00:12:41 +0800

    Move header files into include
    Signed-off-by: Yi Jun <yili0568@gmail.com>

---
+++ include/errno.h
@@ -0,0 +1,17 @@
+/* errno.h standard header */
+#ifndef _ERRNO
+#define _ERRNO
+#ifndef _YVALS
+#include "yvals.h"
+#endif // _ERRNO
+
+    /* error codes */
+#define EDOM        _EDOM
+#define ERANGE      _ERANGE
+#define EFPOS       _EFPOS
+    /* ADD YOURS HERE */
+#define _NERR       _ERRMAX     /* one more than last code */
+    /* declarations */
+extern int errno;
+
+#endif
---
+++ include/xtinfo.h
@@ -0,0 +1,17 @@
+/* xtinfo.h internal header */
+/* type definitions */
+#ifndef _XTINFO
+#define _XTINFO
+
+typedef struct {
+    const char *_Ampm;
+    const char *_Days;
+    const char *_Formats;
+    const char *_Isdst;
+    const char *_Months;
+    const char *_Tzone;
+} _Tinfo ;
+
+/* declarations */
+extern _Tinfo _Times;
+#endif
