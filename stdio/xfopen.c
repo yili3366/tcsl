aa0318a1007bace39469222451d5a0d16cef0717
Yijun <yijun0568@foxmail.com>
2015-04-07 14:42:47 +0800

    Fixed bug about syscall open
    Signed-off-by: Yijun <yijun0568@foxmail.com>

--- stdio/xfopen.c
+++ stdio/xfopen.c
@@ -11,10 +11,13 @@ int _Fopen ( const char *path, unsigned int smode, const char *mode)
     acc = (smode & (_MOPENR|_MOPENW)) == (_MOPENR | _MOPENW) ? 2
         : smode &_MOPENW ? 1 : 0;
     if ( smode & _MOPENA)
-        acc |= 010;  /* O_APPEND */
+        //acc |= 010;  /* O_APPEND */
+        acc |= 00002000;  /* Linux O_APPEND */
     if (smode & _MTRUNC)
-        acc |= 02000;  /* O_TRUNC */
+        //acc |= 02000;  /* O_TRUNC */
+        acc |= 00001000;  /* Linux O_TRUNC */
     if (smode & _MCREAT)
-        acc |= 01000; /* O_CREAT */
+        //acc |= 01000; /* O_CREAT */
+        acc |= 00000100; /* Linux O_CREAT */
     return (_Open(path, acc, 0666)) ;
 }
